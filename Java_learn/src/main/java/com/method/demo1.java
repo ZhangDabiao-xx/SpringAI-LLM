cc093d1bc0eb010362abce18c18011a3a0e84dfc
张大彪 <3052529319@qq.com>
2026-08-12 14:36:41 +0800

    创建java项目

---
+++ Java_learn/src/main/java/com/method/demo1.java
@@ -0,0 +1,13 @@
+package com.method;
+
+public class demo1 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        method(a,b);
+    }
+
+    public static void method(int a,int b){
+        System.out.println(a+b);
+    }
+}
